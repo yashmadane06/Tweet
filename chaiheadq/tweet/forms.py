344c1cb66882b067352657834de6d6e0d7928f6c
unknown <yashmadanelap06.com>
2025-01-10 19:48:18 +0530

    initial commit

---
+++ chaiheadq/tweet/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from . models import Tweet
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class TweetFrom(forms.ModelForm):
+    class Meta:
+        model =Tweet
+        fields =['text','photo']
+
+class UserRegistrationForm(UserCreationForm):
+    email =forms.EmailField
+    class Meta:
+        model =User
+        fields=('username','email','password1','password2')
+
+
+class TweetSearchForm(forms.Form):
+    query = forms.CharField(max_length=100)
